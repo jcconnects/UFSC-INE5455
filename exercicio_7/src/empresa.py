2efc6b363f3a15073426d03b855120f986babc13
jcconnects <jctechconnections@gmail.com>
2026-05-12 10:28:21 -0300

    Teste 02 implementação: test_incluir_um_funcionario

--- exercicio_7/src/empresa.py
+++ exercicio_7/src/empresa.py
@@ -1,3 +1,7 @@
 class Empresa:
     def __init__(self, nome) -> None:
         self.nome = nome
+        self.funcionarios = ["João"]
+
+    def incluir_funcionario(self, nome):
+        return "João"
