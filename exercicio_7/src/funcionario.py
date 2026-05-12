9ce0a5e8d74d0a54a03ec8631922780525997607
breno <brenosilvp@gmail.com>
2026-05-12 10:44:19 -0300

    Refatoração: criação da classe Funcionario

---
+++ exercicio_7/src/funcionario.py
@@ -0,0 +1,4 @@
+class Funcionario:
+    def __init__(self, id, nome) -> None:
+        self.id = id
+        self.nome = nome
--- exercicio_7/tests/tests_file.py
+++ exercicio_7/tests/tests_file.py
@@ -5,6 +5,7 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from src.empresa import Empresa
+from src.funcionario import Funcionario
 
 class Test(unittest.TestCase):
 
@@ -17,15 +18,18 @@ class Test(unittest.TestCase):
     # Teste 02
     def test_incluir_um_funcionario(self):
         empresa = Empresa(nome="WEG")
-        empresa.incluir_funcionario("João")
+        funcionario = Funcionario(id=1, nome="João")
+        empresa.incluir_funcionario(funcionario)
         self.assertEqual(len(empresa.funcionarios), 1)
-        self.assertEqual(empresa.funcionarios[0], "João")
+        self.assertEqual(empresa.funcionarios[0], funcionario)
 
     # Teste 03
     def test_incluir_dois_funcionarios(self):
         empresa = Empresa(nome="WEG")
-        empresa.incluir_funcionario("João")
-        empresa.incluir_funcionario("Maria")
+        funcionario1 = Funcionario(id=1, nome="João")
+        funcionario2 = Funcionario(id=2, nome="Maria")
+        empresa.incluir_funcionario(funcionario1)
+        empresa.incluir_funcionario(funcionario2)
         self.assertEqual(len(empresa.funcionarios), 2)
-        self.assertEqual(empresa.funcionarios[0], "João")
-        self.assertEqual(empresa.funcionarios[1], "Maria")
+        self.assertEqual(empresa.funcionarios[0], funcionario1)
+        self.assertEqual(empresa.funcionarios[1], funcionario2)
