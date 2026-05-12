dc46ebd1bce72ccf028fe6f236b37fb6a6291f67
breno <brenosilvp@gmail.com>
2026-05-12 11:11:11 -0300

    Teste 04 implementação: test_criar_um_projeto

--- exercicio_7/src/empresa.py
+++ exercicio_7/src/empresa.py
@@ -1,13 +1,18 @@
 
 from funcionario import Funcionario
-
+from projeto import Projeto
 class Empresa:
     def __init__(self, nome) -> None:
         self.nome = nome
         self.funcionarios = []
+        self.projetos = []
 
     def criar_funcionario(self, nome):
         funcionario = Funcionario(id=len(self.funcionarios) + 1, nome=nome)
         self.funcionarios.append(funcionario)
         return funcionario
+
+    def criar_projeto(self, nome):
+        proneto = Projeto(id=len(self.projetos) + 1, nome=nome)
+        self.projetos.append(proneto)
 
---
+++ exercicio_7/src/projeto.py
@@ -0,0 +1,4 @@
+class Projeto:
+    def __init__(self, id, nome) -> None:
+        self.id = id
+        self.nome = nome
